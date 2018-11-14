6f0c71aa78affcca2746061db072b221a01987b7
Maxwell <maxperryg@gmail.com>
2018-11-14 17:58:17 -0500

    Initial Commit

---
+++ VNT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VNT
+//
+//  Created by Maxwell Perry Grossman on 11/14/18.
+//  Copyright © 2018 Maxwell Perry Grossman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
